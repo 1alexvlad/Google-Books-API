53673ae582ebf885966969beda3241c2d17b37f8
Vladislav Alexandrov <texr44760@gmail.com>
2026-01-27 21:02:12 +0300

    Add BaseServices class with async find_all/find_one_or_none using SQLAlchemy

---
+++ services/base.py
@@ -0,0 +1,22 @@
+from database import async_session_maker
+
+from sqlalchemy import select 
+
+
+class BaseServices:
+    model = None 
+
+    @classmethod
+    async def find_all(cls, **filter_by):
+        async with async_session_maker() as session:
+            query = select(cls.model).filter_by(**filter_by)
+            result = await session.execute(query)
+            return result.scalars().all()
+        
+    
+    @classmethod
+    async def find_one_or_none(cls, **filter_by):
+        async with async_session_maker() as session:
+            query = select(cls.model).filter_by(**filter_by)
+            result = await session.execute(query)
+            return result.scalar_on_none()
